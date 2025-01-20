66040efd7f2d45f1236dd13cd5a2f85a332115df
manonlebihan <manonlebihan1999@gmail.com>
2025-01-20 11:36:12 +0100

    Implemented task management CLI: mark-in-progress, done

--- task_tracker.py
+++ task_tracker.py
@@ -60,6 +60,30 @@ def delete_task(task_id, file_path=file_path):
             print(f"Task with ID {task_id} was deleted")
             return
     print(f"Task with ID {task_id} not found")
+
+def mark_task_in_progress(task_id, file_path=file_path):
+    tasks = load_tasks(file_path)
+
+    for task in tasks:
+        if task["id"] == task_id:
+            task["status"] = "in progress"
+            task["updatedAt"] = get_current_timestamp()
+            save_tasks(tasks, file_path)
+            print(f"Task with ID {task_id} was marked 'in progress'")
+            return
+    print(f"Task with ID {task_id} not found")
+
+def mark_task_done(task_id, file_path=file_path):
+    tasks = load_tasks(file_path)
+
+    for task in tasks:
+        if task["id"] == task_id:
+            task["status"] = "done"
+            task["updatedAt"] = get_current_timestamp()
+            save_tasks(tasks, file_path)
+            print(f"Task with ID {task_id} was marked 'done'")
+            return
+    print(f"Task with ID {task_id} not found")
     
 
 def main():
@@ -79,6 +103,14 @@ def main():
     parser_update = subparsers.add_parser("delete", help="Delete a task")
     parser_update.add_argument("id", type=int, help="ID of the task")
 
+    # Mark a task as in-progress
+    parser_in_progress = subparsers.add_parser("mark-in-progress", help="Mark a task as in progress")
+    parser_in_progress.add_argument("id", type=int, help="ID of the task")
+
+    # Mark a task as done
+    parser_done = subparsers.add_parser("mark-done", help="Mark a task as done")
+    parser_done.add_argument("id", type=int, help="ID of the task")
+
     args = parser.parse_args()
     if args.command == "add":
         add_task(args.title)
@@ -86,6 +118,10 @@ def main():
         update_task(args.id, args.title)
     elif args.command == "delete":
         delete_task(args.id)
+    elif args.command == "mark-in-progress":
+        mark_task_in_progress(args.id)
+    elif args.command == "mark-done":
+        mark_task_done(args.id)
     # Call appropriate functions based on `args.command`
 
 main()
